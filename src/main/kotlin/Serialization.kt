ed858ef11dea9a9e863a6875364c5d0ad386ae1f
Philip <psegerfast@gmail.com>
2025-04-04 14:15:26 +0200

    Initial commit

---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,20 @@
+package dev.psegerfast
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.jetbrains.exposed.sql.*
+import org.slf4j.event.*
+
+fun Application.configureSerialization() {
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
